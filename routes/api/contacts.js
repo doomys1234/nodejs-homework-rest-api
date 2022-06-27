c92735d9324d75ea6119834f0aafb1b0b92815ea
doomys1234 <doomys1234@gmail.com>
2022-06-27 15:19:47 +0300

    ready

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,112 @@
-const express = require('express')
+const express = require("express");
+const shortid = require("shortid");
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} = require("../../models/contacts");
+const router = express.Router();
+const Joi = require("joi");
 
-const router = express.Router()
+router.get("/", async (req, res, next) => {
+  const data = await listContacts();
+  res.status(200).json({
+    status: "Successful",
+    data: data,
+  });
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const item = await getContactById(contactId);
+  if (item.length === 0) {
+    res.status(404).json({
+      status: "Error",
+      message: `Contact with ${contactId} does not exist`,
+    });
+  }
+  res.status(200).json({
+    status: "Successful",
+    data: item,
+  });
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+      .required(),
+    phone: Joi.string().alphanum().min(3).max(30).required(),
+  });
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const validation = schema.validate(req.body);
+  if (validation.error) {
+    return res.status(400).json({
+      status: "Error",
+      message: validation.error.details[0].message,
+    });
+  }
+  const { name, email, phone } = req.body;
+  const newItem = {
+    id: shortid(),
+    name: name,
+    email: email,
+    phone: phone,
+  };
+  await addContact(newItem);
+  res.status(201).json({
+    status: "Successfully added",
+    item: newItem,
+  });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const removedContact = await removeContact(contactId);
+  if (!removedContact) {
+    res.status(404).json({
+      status: "Error",
+      message: `Contact with id:${contactId} does not exist`,
+    });
+  }
+  res.status(200).json({
+    status: "Successful",
+    message: `Contact deleted`,
+    removedContact: removedContact,
+  });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+      .required(),
+    phone: Joi.string().alphanum().min(3).max(30).required(),
+  });
 
-module.exports = router
+  const validation = schema.validate(req.body);
+  if (validation.error) {
+    return res.status(400).json({
+      status: "Error",
+      message: validation.error.details[0].message,
+    });
+  }
+  const { contactId } = req.params;
+  const updateditem = await updateContact(contactId, req.body);
+  if (!updateditem) {
+    res.status(404).json({
+      status: "Error",
+      message: `Contact with id:${contactId} does not exist`,
+    });
+  }
+  res.status(200).json({
+    status: "Successfully updated",
+    updateditem: updateditem,
+  });
+});
+
+module.exports = router;
