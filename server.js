19698324559912e03a85c7784d2fbba3150961ae
doomys1234 <doomys1234@gmail.com>
2022-08-10 15:18:20 +0300

    ready

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const app = require('./app')
+const { connectMongo } = require("./db/connection")
 
-app.listen(3000, () => {
+const startServer = async () => {
+  try {
+    await connectMongo()
+    console.log('Database connection successful');
+    app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
 })
+  } catch (error) {
+    console.log("error to connect to mongo");
+  }
+}
+
+
+startServer()
+module.exports={startServer}
